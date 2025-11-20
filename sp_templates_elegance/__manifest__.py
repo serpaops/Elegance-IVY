14baf13a0bd289f64b96adcb32c5a888e1c2e79c
Bethel2222 <bchigwanda2@gmail.com>
2025-11-20 09:30:19 +0200

    elegance templates initial commit.

---
+++ sp_templates_elegance/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Templates",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'account', 'stock', 'sale_management'],
+
+    'data': [
+        'views/templates.xml',
+    ],
+}
+
